f5b44cfa5ead655058e6f625311cd31a1d56426d
Eshaan, K <eshaan.katla@gmail.com>
2025-01-01 16:23:56 +0530

    Maven files

---
+++ src/main/java/org/app/demo/ProjectMaven/App.java
@@ -0,0 +1,11 @@
+package org.app.demo.ProjectMaven;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello Eshaan");
+        System.out.println("This is your second Maven Project");
+        }
+}
